3ac8d24bbe2f481298f30f90b0f6731a7d3e07ec
Doğukan ÖZEL <dogukanozel692@gmail.com>
2023-10-31 18:14:07 +0300

    Yazı Tura Uygulaması Yapıldı.

--- GorselP2Ders6/Form1.Designer.cs
+++ GorselP2Ders6/Form1.Designer.cs
@@ -28,13 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnat = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnçık = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnat
+            // 
+            this.btnat.Location = new System.Drawing.Point(6, 32);
+            this.btnat.Name = "btnat";
+            this.btnat.Size = new System.Drawing.Size(101, 87);
+            this.btnat.TabIndex = 1;
+            this.btnat.Text = "Yazı Tura At";
+            this.btnat.UseVisualStyleBackColor = true;
+            this.btnat.Click += new System.EventHandler(this.btnat_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnat);
+            this.groupBox1.Controls.Add(this.btnçık);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 290);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 251);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // btnçık
+            // 
+            this.btnçık.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnçık.Location = new System.Drawing.Point(6, 125);
+            this.btnçık.Name = "btnçık";
+            this.btnçık.Size = new System.Drawing.Size(101, 103);
+            this.btnçık.TabIndex = 2;
+            this.btnçık.UseVisualStyleBackColor = true;
+            this.btnçık.Click += new System.EventHandler(this.btnçık_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(123, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 142);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnat;
+        private System.Windows.Forms.Button btnçık;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- GorselP2Ders6/Form1.cs
+++ GorselP2Ders6/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,65 @@ namespace GorselP2Ders6
         {
             InitializeComponent();
         }
+        #region Uygulama Yazı Tura.
+     
+        private void btnat_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = Image.FromFile("yazi.jpeg"); //resimekleme
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch; //Resime göre stretch ayarını kodla yapımı.
+            btnçık.BackgroundImage = Image.FromFile("images.jpg"); //Resim ekleme
+            btnçık.BackgroundImageLayout = ImageLayout.Stretch;  //Resime göre stretch ayarını kodla yapımı.
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Random rnd = new Random();
+            int gelen = rnd.Next(1,3);
+            if (gelen == 1)
+            {
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpeg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpeg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpeg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpeg");
+            }
+            
+            else
+            {
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpeg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpeg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("yazi.jpeg");
+                System.Threading.Thread.Sleep(250);
+                pictureBox1.BackgroundImage = Image.FromFile("tura.jpg");
+            }
+        }
+
+        private void btnçık_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+       
     }
 }
